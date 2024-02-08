35bbcc347424c6e43262a465035c9aeeb4d97fa9
moqs <moqs001@gmail.com>
2024-02-08 21:43:57 +0100

    fix: ci with webpack copy plugin

--- libs/wrapper/webpack.config.ts
+++ libs/wrapper/webpack.config.ts
@@ -1,6 +1,6 @@
 import { composePlugins, withNx } from '@nx/webpack';
 import { IgnorePlugin, SourceMapDevToolPlugin } from 'webpack';
-
+import * as CopyPlugin from 'copy-webpack-plugin';
 // Nx plugins for webpack.
 module.exports = composePlugins(withNx(), (config) => {
   // Update the webpack config as needed here.
@@ -13,10 +13,14 @@ module.exports = composePlugins(withNx(), (config) => {
       exclude: ['vendors.js'],
       moduleFilenameTemplate: '../[namespace]/[resource-path]',
     }),
+    new CopyPlugin({
+      patterns: [
+        { from: 'package.json', to: 'package.json' },
+      ],
+    }),
     new IgnorePlugin({
       checkResource(resource) {
-        const lazyImports = [
-        ];
+        const lazyImports = [];
 
         if (!lazyImports.includes(resource)) {
           return false;
