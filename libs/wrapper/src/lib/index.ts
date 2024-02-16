8343aa643c8d6bda297a950090f2efb91c57d32b
moqs <moqs001@gmail.com>
2024-02-16 21:05:47 +0100

    fix: export path

--- libs/wrapper/src/lib/index.ts
+++ libs/wrapper/src/lib/index.ts
@@ -1,5 +1,2 @@
+export * from './core';
 export * from './wrapper';
-export * from '@godcli/core'
-export * from '@godcli/plugins/ansible'
-export * from '@godcli/plugins/core'
-export * from '@godcli/plugins/interfaces'
---
+++ libs/wrapper/src/lib/plugins/ansible/index.ts
@@ -0,0 +1,1 @@
+export * from '@godcli/plugins/ansible'
